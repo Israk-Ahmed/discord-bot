7b401d35e57b9b541da86d6c1178278bc7f5c0a4
Israk Ahmed <60624661+Israk-Ahmed@users.noreply.github.com>
2021-10-08 23:54:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ async def clear(ctx, amount=5):
     await ctx.send("Messages have been cleared")
 
 
-my_secret = os.environ['TOKEN']
+#my_secret = os.environ['TOKEN']
 
-client.run(my_secret)
+client.run(ODk2MDkyMzY2MzcxNDIyMjUw.YWCFDA.VZVcaFI8R9apOAPNWgaFYsLa-vo)
 
